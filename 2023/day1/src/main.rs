aabc939420506776b4fcea56882cecfa8d0b824e
Juliette Cordor <juliette@jewelexx.com>
2023-12-03 20:33:16 +1100

    updates

--- 2023/day1/src/main.rs
+++ 2023/day1/src/main.rs
@@ -1,21 +1,102 @@
 const INPUT: &str = include_str!("../input.txt");
 
-fn main() {
-    let calibration_value_sum = INPUT
+fn i_to_num(i: usize) -> usize {
+    (i % (POSSIBLE_DIGITS.len() / 2)) + 1
+}
+
+#[rustfmt::skip]
+pub const POSSIBLE_DIGITS: [&str; 18] = [
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "one",
+    "two",
+    "three",
+    "four",
+    "five",
+    "six",
+    "seven",
+    "eight",
+    "nine",
+];
+
+pub fn sum_lines(input: &str) -> u32 {
+    input
         .lines()
         .map(|line| {
-            line.chars()
-                .filter(char::is_ascii_digit)
-                .collect::<Vec<_>>()
-        })
-        .filter_map(|line_numbers| {
-            let first = line_numbers.first()?;
-            let last = line_numbers.last()?;
+            let locations = POSSIBLE_DIGITS
+                .iter()
+                .enumerate()
+                .filter_map(|(i, digit)| line.find(digit).map(|pos| (pos, i_to_num(i))))
+                .collect::<Vec<_>>();
+
+            let first = locations.iter().min_by_key(|d| d.0).unwrap();
+            let last = locations.iter().max_by_key(|d| d.0).unwrap();
+
+            if locations.len() == 1 {
+                assert_eq!(first, last);
+            } else {
+                assert_ne!(first, last);
+            }
+
+            let first = first.1;
+            let last = last.1;
             let combined = format!("{first}{last}");
 
-            Some(combined.parse::<u32>().unwrap())
+            combined.parse::<u32>().unwrap()
         })
-        .sum::<u32>();
+        .sum::<u32>()
+}
+
+fn main() {
+    let calibration_value_sum = sum_lines(INPUT);
 
     println!("The sum of all the calibration values is {calibration_value_sum}");
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_examples() {
+        let sum = sum_lines(
+            "two1nine
+eightwothree
+abcone2threexyz
+xtwone3four
+4nineeightseven2
+zoneight234
+7pqrstsixteen",
+        );
+
+        assert_eq!(sum, 281);
+    }
+
+    #[test]
+    fn test_digits() {
+        for (i, digit) in POSSIBLE_DIGITS.iter().enumerate() {
+            let predicted = i_to_num(i);
+            let actual = match *digit {
+                "1" | "one" => 1,
+                "2" | "two" => 2,
+                "3" | "three" => 3,
+                "4" | "four" => 4,
+                "5" | "five" => 5,
+                "6" | "six" => 6,
+                "7" | "seven" => 7,
+                "8" | "eight" => 8,
+                "9" | "nine" => 9,
+                _ => unreachable!(),
+            };
+
+            assert_eq!(predicted, actual);
+        }
+    }
 }
---
+++ picker/src/main.rs
@@ -0,0 +1,50 @@
+//! Picks a random language for you to complete the next day in
+//! Inspired by Low Level Learning's videos (https://www.youtube.com/watch?v=t9CKGXov2Z4)
+
+use std::{io::Write, thread::sleep, time::Duration};
+
+use rand::Rng;
+use rand_derive2::RandGen;
+
+#[derive(Debug, strum::Display, RandGen)]
+pub enum Language {
+    Rust,
+    Cpp,
+    C,
+    Javascript,
+    Typescript,
+    PHP,
+    Crystal,
+    Haskell,
+    Zig,
+    Ruby,
+    Go,
+    OCaml,
+    Clojure,
+    Java,
+    Nim,
+    Python,
+    Dart,
+}
+
+fn main() {
+    println!("Todays language is...");
+    sleep(Duration::from_millis(230));
+    println!("Drumroll please...");
+
+    let delay_time = {
+        let mut rng = rand::thread_rng();
+        rng.gen_range(250..750)
+    };
+
+    for _ in 0..3 {
+        print!(".");
+        std::io::stdout().lock().flush().unwrap();
+        sleep(Duration::from_millis(delay_time));
+    }
+    println!();
+
+    let language: Language = rand::random();
+
+    println!("{language}!!!!!");
+}
