70bf497b2e08652fe56f3e412630c645aec5c850
Juliette Cordor <juliette@jewelexx.com>
2023-12-03 18:57:56 +1100

    completed day one

--- 2023/day1/src/main.rs
+++ 2023/day1/src/main.rs
@@ -1,3 +1,21 @@
+const INPUT: &str = include_str!("../input.txt");
+
 fn main() {
-    println!("Hello, world!");
+    let calibration_value_sum = INPUT
+        .lines()
+        .map(|line| {
+            line.chars()
+                .filter(char::is_ascii_digit)
+                .collect::<Vec<_>>()
+        })
+        .filter_map(|line_numbers| {
+            let first = line_numbers.first()?;
+            let last = line_numbers.last()?;
+            let combined = format!("{first}{last}");
+
+            Some(combined.parse::<u32>().unwrap())
+        })
+        .sum::<u32>();
+
+    println!("The sum of all the calibration values is {calibration_value_sum}");
 }
