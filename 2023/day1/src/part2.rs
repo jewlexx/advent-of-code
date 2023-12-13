f56b2af34061b3bf6ded67a248f0caa1cee7e1f4
Juliette Cordor <juliette@jewelexx.com>
2023-12-13 13:19:15 +1100

    fixed finding digits in line

--- 2023/day1/src/part2.rs
+++ 2023/day1/src/part2.rs
@@ -47,8 +47,11 @@ impl EndOffset {
         Self::END_OFFSETS[i]
     }
 
-    pub fn apply(self) {
-        todo!()
+    pub fn apply(self, length: usize) -> usize {
+        let mut length = length as isize;
+        length += self.end;
+
+        length as usize
     }
 }
 
@@ -59,14 +62,45 @@ pub fn str_to_digit(str: &str) -> Option<(usize, EndOffset)> {
         .map(|i| ((i % 9) + 1, EndOffset::get(i)))
 }
 
+// This does not consider digits that are substrings
+pub fn find_digits_in_line_old(line: &str) -> Vec<usize> {
+    let chars = line.chars();
+
+    let mut current_string = String::new();
+    let mut found_digits = vec![];
+
+    for c in chars {
+        current_string.push(c);
+
+        if let Some((digit, end_offset)) = str_to_digit(&current_string) {
+            found_digits.push(digit);
+            let new_start = end_offset.apply(current_string.len());
+            current_string = String::from(&current_string[new_start..]);
+        }
+    }
+
+    found_digits
+}
+
+pub fn find_digits_in_line(line: &str) -> Vec<usize> {
+    // Obviously this solution is not super optimized
+    // There are obvious improvements to be made where we could filter all digits that are longer than the string we have available
+    // But for this case, it's not that important
+    let mut found_digits = POSSIBLE_DIGITS
+        .into_iter()
+        .enumerate()
+        .filter_map(|(i, digit)| line.find(digit).map(|pos| ((i % 9) + 1, pos)))
+        .collect::<Vec<(usize, usize)>>();
+
+    found_digits.sort_by_key(|(_, i)| *i);
+
+    found_digits.into_iter().map(|(number, _)| number).collect()
+}
+
 pub fn run(input: &str) -> u32 {
     input
         .lines()
-        .map(|line| {
-            line.chars()
-                .filter(char::is_ascii_digit)
-                .collect::<Vec<_>>()
-        })
+        .map(find_digits_in_line)
         .filter_map(|line_numbers| {
             let first = line_numbers.first()?;
             let last = line_numbers.last()?;
@@ -102,5 +136,11 @@ zoneight234
 
             assert_eq!(number, str_to_digit(digit).unwrap().0);
         }
+    }
+
+    #[test]
+    fn test_digits_in_line() {
+        let digits = find_digits_in_line("two1nine");
+        assert_eq!(digits, [2, 1, 9])
     }
 }
