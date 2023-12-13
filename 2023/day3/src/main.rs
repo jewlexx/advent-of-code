046905f2ebeabbc0907e0176f6d242a1d52d54a7
Juliette Cordor <juliette@jewelexx.com>
2023-12-13 13:30:58 +1100

    added day3

---
+++ 2023/day3/src/main.rs
@@ -0,0 +1,9 @@
+const INPUT: &str = include_str!("../input.txt");
+
+mod part1;
+mod part2;
+
+fn main() {
+    println!("Answer to part 1 is: {}", part1::run(INPUT));
+    println!("Answer to part 2 is: {}", part2::run(INPUT));
+}
